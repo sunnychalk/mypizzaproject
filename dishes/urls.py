d45f863081dd2967d1366280bbcf34f24a878fea
Olga Karaivan <olga.karaivan1@gmail.com>
2019-11-10 17:04:52 +0200

    added form view, create view

--- dishes/urls.py
+++ dishes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'menu', views.DishesView.as_view()),
     url(r'about', views.MyView.as_view()),
     url(r'drink_list', views.DrinksListView.as_view()),
+    url(r'make_order/', views.MakeOrder.as_view()),
+    url(r'dish_form', views.DishCreate.as_view()),
 ]
--- dishes/views.py
+++ dishes/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView, CreateView
 from dishes.models import Dish, Drink
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from dishes.forms import ContactForm, DrinkForm
 
 # Create your views here.
 class MyView(View):
@@ -24,3 +26,19 @@ class DishesView(TemplateView):
 		context = super(DishesView,self).get_context_data(**kwargs)
 		context['dishes'] = Dish.objects.all().order_by('name')
 		return context
+
+
+class MakeOrder(FormView):
+	template_name = 'make_order.html'
+	form_class = ContactForm
+	success_url = '/'
+
+	def form_valid(self, form):
+		form.send_email()
+		return super(ContactView).form_valid(form)
+
+
+class DishCreate(CreateView):
+	template_name = 'dish_form.html'
+	model = Dish
+	fields = ['name', 'ingridients', 'is_meat', 'is_vegan', 'price']
